937522829d8c7cf41d71cdb142425977837b4ceb
Вячеслав Кузнецов <kuznetsov1997Vyacheslav@yandex.ru>
2023-01-13 14:45:28 +0300

    Update

--- DZ_2/WinApiFunctions.cs
+++ DZ_2/WinApiFunctions.cs
@@ -12,13 +12,13 @@ namespace DZ_2
         [DllImport("User32.dll")]
         public static extern int MessageBox(IntPtr hwnd, string message, string caption, uint decoration);
 
-        [DllImport("user32.dll")]
+        [DllImport("User32.dll")]
         public static extern IntPtr GetForegroundWindow();
 
-        [DllImport("user32.dll")]
+        [DllImport("User32.dll")]
         public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
         
-        [DllImport("user32.dll")]
+        [DllImport("User32.dll")]
         public static extern int GetWindowTextLength(IntPtr hWnd);
     }
 }
